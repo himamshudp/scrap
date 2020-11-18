d4e2ec2b3de00dd0a777e37261c9641664e2aedd
Himanshu Sharma <69843278+himamshudp@users.noreply.github.com>
2020-11-18 11:37:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,10 @@ def dmart():
         columns=['Product_category', 'product_name', 'Product_o_price', 'product_d_price', 'p_brand'])
     product_url_try = product_url[:3]
     # headless chorme
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     #chromedriver = 'chromedriver.exe'
