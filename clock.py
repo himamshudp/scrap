20df88716e9563b5fe4a633d2271f44394fd89b0
Himanshu Sharma <69843278+himamshudp@users.noreply.github.com>
2020-11-19 11:16:58 +0530

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,9 @@
+from app2 import dmart
+import schedule
+import time
+# Task scheduling After every 10mins geeks() is called.
+schedule.every(5).minutes.do(dmart)
+while True :
+    # Checks whether a scheduled task is pending to run or not
+    schedule.run_pending()
+    time.sleep(1)
