3a3488f86eda3001260fb5c1d3ec403ea39fde8f
Himanshu Sharma <69843278+himamshudp@users.noreply.github.com>
2020-11-20 13:24:27 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -2,7 +2,9 @@ from app2 import dmart
 import schedule
 import time
 # Task scheduling After every 10mins geeks() is called.
+print("calling dmart")
 dmart()
+print("calling scheduler")
 schedule.every(5).minutes.do(dmart)
 while True :
     # Checks whether a scheduled task is pending to run or not
